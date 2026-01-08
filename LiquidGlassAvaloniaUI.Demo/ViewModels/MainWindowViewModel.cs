5039ef6f1dca44e953181e2eaa9a39ca670df93c
KaranocaVe <karanocave@qq.com>
2026-01-09 00:24:23 +0800

    demo: update demo app with advanced LiquidGlass controls

--- LiquidGlassAvaloniaUI.Demo/ViewModels/MainWindowViewModel.cs
+++ LiquidGlassAvaloniaUI.Demo/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Avalonia;
 using Avalonia.Media;
 using ReactiveUI;
 
@@ -10,11 +11,38 @@ public class MainWindowViewModel : ViewModelBase
     private double _refractionAmount = 24.0;
     private double _blurRadius = 2.0;
     private double _vibrancy = 1.5;
+    private double _brightness = 0.0;
+    private double _contrast = 1.0;
+    private double _exposureEv = 0.0;
+    private double _gammaPower = 1.0;
+    private double _backdropOpacity = 1.0;
     private bool _depthEffect;
     private bool _chromaticAberration;
 
     private bool _highlightEnabled = true;
+    private double _highlightWidth = 0.5;
+    private double _highlightBlurRadius = 0.25;
     private double _highlightOpacity = 0.5;
+    private double _highlightAngle = 45.0;
+    private double _highlightFalloff = 1.0;
+
+    private bool _shadowEnabled = true;
+    private double _shadowRadius = 24.0;
+    private double _shadowOffsetX = 0.0;
+    private double _shadowOffsetY = 4.0;
+    private double _shadowOpacity = 1.0;
+    private Color _shadowColor = Color.FromArgb(26, 0, 0, 0);
+
+    private bool _innerShadowEnabled;
+    private double _innerShadowRadius = 24.0;
+    private double _innerShadowOffsetX = 0.0;
+    private double _innerShadowOffsetY = 24.0;
+    private double _innerShadowOpacity = 1.0;
+    private Color _innerShadowColor = Color.FromArgb(38, 0, 0, 0);
+
+    private bool _isInteractive = true;
+    private bool _interactiveHighlightEnabled = true;
+    private double _interactiveMaxScaleDip = 4.0;
 
     private bool _enableTint;
     private int _tintPresetIndex = 0;
@@ -45,6 +73,36 @@ public class MainWindowViewModel : ViewModelBase
         set => this.RaiseAndSetIfChanged(ref _vibrancy, value);
     }
 
+    public double Brightness
+    {
+        get => _brightness;
+        set => this.RaiseAndSetIfChanged(ref _brightness, value);
+    }
+
+    public double Contrast
+    {
+        get => _contrast;
+        set => this.RaiseAndSetIfChanged(ref _contrast, value);
+    }
+
+    public double ExposureEv
+    {
+        get => _exposureEv;
+        set => this.RaiseAndSetIfChanged(ref _exposureEv, value);
+    }
+
+    public double GammaPower
+    {
+        get => _gammaPower;
+        set => this.RaiseAndSetIfChanged(ref _gammaPower, value);
+    }
+
+    public double BackdropOpacity
+    {
+        get => _backdropOpacity;
+        set => this.RaiseAndSetIfChanged(ref _backdropOpacity, value);
+    }
+
     public bool DepthEffect
     {
         get => _depthEffect;
@@ -63,10 +121,144 @@ public class MainWindowViewModel : ViewModelBase
         set => this.RaiseAndSetIfChanged(ref _highlightEnabled, value);
     }
 
+    public double HighlightWidth
+    {
+        get => _highlightWidth;
+        set => this.RaiseAndSetIfChanged(ref _highlightWidth, value);
+    }
+
+    public double HighlightBlurRadius
+    {
+        get => _highlightBlurRadius;
+        set => this.RaiseAndSetIfChanged(ref _highlightBlurRadius, value);
+    }
+
     public double HighlightOpacity
     {
         get => _highlightOpacity;
         set => this.RaiseAndSetIfChanged(ref _highlightOpacity, value);
+    }
+
+    public double HighlightAngle
+    {
+        get => _highlightAngle;
+        set => this.RaiseAndSetIfChanged(ref _highlightAngle, value);
+    }
+
+    public double HighlightFalloff
+    {
+        get => _highlightFalloff;
+        set => this.RaiseAndSetIfChanged(ref _highlightFalloff, value);
+    }
+
+    public bool ShadowEnabled
+    {
+        get => _shadowEnabled;
+        set => this.RaiseAndSetIfChanged(ref _shadowEnabled, value);
+    }
+
+    public double ShadowRadius
+    {
+        get => _shadowRadius;
+        set => this.RaiseAndSetIfChanged(ref _shadowRadius, value);
+    }
+
+    public double ShadowOffsetX
+    {
+        get => _shadowOffsetX;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _shadowOffsetX, value);
+            this.RaisePropertyChanged(nameof(ShadowOffset));
+        }
+    }
+
+    public double ShadowOffsetY
+    {
+        get => _shadowOffsetY;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _shadowOffsetY, value);
+            this.RaisePropertyChanged(nameof(ShadowOffset));
+        }
+    }
+
+    public Vector ShadowOffset => new Vector(ShadowOffsetX, ShadowOffsetY);
+
+    public double ShadowOpacity
+    {
+        get => _shadowOpacity;
+        set => this.RaiseAndSetIfChanged(ref _shadowOpacity, value);
+    }
+
+    public Color ShadowColor
+    {
+        get => _shadowColor;
+        set => this.RaiseAndSetIfChanged(ref _shadowColor, value);
+    }
+
+    public bool InnerShadowEnabled
+    {
+        get => _innerShadowEnabled;
+        set => this.RaiseAndSetIfChanged(ref _innerShadowEnabled, value);
+    }
+
+    public double InnerShadowRadius
+    {
+        get => _innerShadowRadius;
+        set => this.RaiseAndSetIfChanged(ref _innerShadowRadius, value);
+    }
+
+    public double InnerShadowOffsetX
+    {
+        get => _innerShadowOffsetX;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _innerShadowOffsetX, value);
+            this.RaisePropertyChanged(nameof(InnerShadowOffset));
+        }
+    }
+
+    public double InnerShadowOffsetY
+    {
+        get => _innerShadowOffsetY;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _innerShadowOffsetY, value);
+            this.RaisePropertyChanged(nameof(InnerShadowOffset));
+        }
+    }
+
+    public Vector InnerShadowOffset => new Vector(InnerShadowOffsetX, InnerShadowOffsetY);
+
+    public double InnerShadowOpacity
+    {
+        get => _innerShadowOpacity;
+        set => this.RaiseAndSetIfChanged(ref _innerShadowOpacity, value);
+    }
+
+    public Color InnerShadowColor
+    {
+        get => _innerShadowColor;
+        set => this.RaiseAndSetIfChanged(ref _innerShadowColor, value);
+    }
+
+    public bool IsInteractive
+    {
+        get => _isInteractive;
+        set => this.RaiseAndSetIfChanged(ref _isInteractive, value);
+    }
+
+    public bool InteractiveHighlightEnabled
+    {
+        get => _interactiveHighlightEnabled;
+        set => this.RaiseAndSetIfChanged(ref _interactiveHighlightEnabled, value);
+    }
+
+    public double InteractiveMaxScaleDip
+    {
+        get => _interactiveMaxScaleDip;
+        set => this.RaiseAndSetIfChanged(ref _interactiveMaxScaleDip, value);
     }
 
     public bool EnableTint
